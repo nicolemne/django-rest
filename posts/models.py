2aaecaaeb3785bee71bb1c86badbf8e5a2141294
Nicole MN.E <mcnultyengstrom@gmail.com>
2023-09-17 05:35:19 +0000

    Added image_filter choice field and validation

--- posts/models.py
+++ posts/models.py
@@ -6,6 +6,15 @@ class Post(models.Model):
     Post model, related to 'owner', i.e, a User Instance.
     Default image set to tat we can always reference image.url
     '''
+    image_filter_choices = image_filter_choices = [
+        ('_1977', '1977'), ('brannan', 'Brannan'),
+        ('earlybird', 'Earlybird'), ('hudson', 'Hudson'),
+        ('inkwell', 'Inkwell'), ('lofi', 'Lo-Fi'),
+        ('kelvin', 'Kelvin'), ('normal', 'Normal'),
+        ('nashville', 'Nashville'), ('rise', 'Rise'),
+        ('toaster', 'Toaster'), ('valencia', 'Valencia'),
+        ('walden', 'Walden'), ('xpro2', 'X-pro II')
+    ]
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
@@ -14,6 +23,9 @@ class Post(models.Model):
     image = models.ImageField(
         upload_to='images/', default='../default_profile_ld91aw', 
         blank=True
+    )
+    image_filter = models.CharField(
+        max_length=32, choices=image_filter_choices, default='normal'
     )
 
     class Meta: 
--- posts/serializers.py
+++ posts/serializers.py
@@ -7,6 +7,21 @@ class PostSerializer(serializers.ModelSerializer):
     profile_id = serializer.ReadOnlyField(source='owner.profile.id')
     profile_image = serializerReadOnlyField(source='owner.profile.image.url')
 
+    def validate_image(self, value):
+        if value.size > 1024 * 1024 * 2:
+            raise serializers.ValidationError(
+                'Image size larger than 2MB'
+            )
+        if value.image.width > 4096: 
+            raise serializers.ValidationError(
+                'Image width larger than 4096px'
+            )
+        if value.image.height > 4096: 
+            raise serializers.ValidationError(
+                'Image height larger than 4096px'
+            )
+        return value
+
     def get_is_owner(self, obj):
         request = self.context['request']
         return request.user == obj.owner
@@ -15,5 +30,6 @@ class PostSerializer(serializers.ModelSerializer):
         model = Post
         fields = [
             'id', 'owner', 'is_owner', 'profile_id', 'profile_image',
-            'created_at', 'updated_at', 'title', 'content', 'image'
+            'created_at', 'updated_at', 'title', 'content', 'image', 
+            'image_filter',
         ]

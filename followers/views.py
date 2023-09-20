1c044ea52d08563007b4480629966776c812969a
Nicole MN.E <mcnultyengstrom@gmail.com>
2023-09-20 23:01:01 +0000

    Refactor Profile&Post views to generic APIView

---
+++ followers/views.py
@@ -0,0 +1,29 @@
+from rest_framework import generics, permissions
+from drf_api.permissions import IsOwnerOrReadOnly
+from followers.models import Follower
+from followers.serializers import FollowerSerializer
+
+
+class FollowerList(generics.ListCreateAPIView):
+    """
+    List all followers, i.e. all instances of a user
+    following another user'.
+    Create a follower, i.e. follow a user if logged in.
+    Perform_create: associate the current logged in user with a follower.
+    """
+    serializer_class = FollowerSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+    def perform_create(self, serializer): 
+        serializer.save(owner=self.request.user)
+
+
+class FollowerDetail(generics.RetrieveDestroyAPIView):
+    """
+    Retrieve a follower
+    No Update view, as we either follow or unfollow users
+    Destroy a follower, i.e. unfollow someone if owner
+    """
+    serializer_class = FollowerSerializer
+    permission_classes = [IsOwnerOrReadOnly]
+    queryset = Follower.objects.all()

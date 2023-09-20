b6c5109cbf7b620479341b9a5429bf967771ca96
Nicole MN.E <mcnultyengstrom@gmail.com>
2023-09-20 23:05:30 +0000

    Added urls to follower views

--- followers/views.py
+++ followers/views.py
@@ -13,6 +13,7 @@ class FollowerList(generics.ListCreateAPIView):
     """
     serializer_class = FollowerSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    queryset = Follower.objects.all()
 
     def perform_create(self, serializer): 
         serializer.save(owner=self.request.user)

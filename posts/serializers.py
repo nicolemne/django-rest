4cb7e044215ab5920ce2846c381df33c531aec24
Nicole MN.E <mcnultyengstrom@gmail.com>
2023-09-21 18:45:45 +0000

    Added the new seralizer counts to fields

--- posts/serializers.py
+++ posts/serializers.py
@@ -44,5 +44,5 @@ class PostSerializer(serializers.ModelSerializer):
             'id', 'owner', 'is_owner', 'profile_id',
             'profile_image', 'created_at', 'updated_at',
             'title', 'content', 'image', 'image_filter',
-            'like_id',
+            'like_id', 'likes_count', 'comments_count',
         ]
